3c2bb72384beafc6bf374769be5a6bc5d92b6296
notnarayan <notnarayan@gmail.com>
2016-01-21 10:22:48 -0500

    testing git pushes

--- StandUp 3/TableViewController.swift
+++ StandUp 3/TableViewController.swift
@@ -15,6 +15,7 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("This is me")
         
         if NSUserDefaults.standardUserDefaults().objectForKey("todolist") != nil {
             
